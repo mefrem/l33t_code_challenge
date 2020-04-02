f5276e23f7567de5e6689d57d34a94409cc83399
mefrem <maxim.efremov@gmail.com>
2020-04-02 18:53:17 -0400

    Day 2 of 30

---
+++ day2_isHappy.py
@@ -0,0 +1,35 @@
+"""
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process:
+Starting with any positive integer, replace the number by the
+sum of the squares of its digits, and repeat the process until
+the number equals 1 (where it will stay), or it loops
+endlessly in a cycle which does not include 1. Those numbers
+for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+"""
+# Original solution. Apparently better than only 5% of others lol
+def isHappy(self, n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    for _ in range(10):
+        new_num = 0
+        for i in str(n):
+            print(n,i)
+            new_num += int(i)**2
+        if new_num == 1:
+            return True
+        n = new_num
+    return False
